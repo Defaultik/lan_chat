fc7748a49b19e1567b11208fb6c2e72ef095754d
Defaultik <defaultik.personal@gmail.com>
2024-03-16 19:16:03 +0200

    messages transferring via objects; windows notifications; init function

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
-import threading, argparse, pickle, socket, sys, os
+import pygetwindow, threading, argparse, pickle, socket, sys, os
+from windows_toasts import Toast, InteractableWindowsToaster
 from datetime import datetime
 
 
@@ -15,18 +16,27 @@ def nickname_creation():
     return nickname
 
 
-def main():
+def init():
+    global parser, window_title
+    
     # creates an argument parser to intercept the arguments that user enters when calling a file
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-i", "--ip", required=True, help="ip of the server socket")
     parser.add_argument("-p", "--port", type=int, help="port of the server socket (0-65535)")
 
+    clear_screen()
+
     if not parser.parse_args().ip:
         parser.error("IP (-i) of the socket wasn't given")
 
+    window_title = pygetwindow.getActiveWindowTitle()
+
+    main()
+
+
+def main():
     nickname = nickname_creation()
-    clear_screen()
 
     try:
         if not parser.parse_args().port:
@@ -34,6 +44,7 @@ def main():
             Client(parser.parse_args().ip, 8383, nickname).connect()
         else:
             Client(parser.parse_args().ip, parser.parse_args.port, nickname).connect()
+
     except socket.error as e:
         print("Socket Error\n%s" % e)
 
@@ -56,8 +67,10 @@ class Client:
             
             threading.Thread(target=self.receive).start() # starts a loop to receive messages in a new thread
             threading.Thread(target=self.send).start() # starts a loop to send messages in a new thread
+
         except socket.error as e:
             print("Connecting error: %s" % e)
+
         except (KeyboardInterrupt, SystemExit):
             print("[%s] You disconnected from the server!" % datetime.now().strftime("%H:%M"))
             self.socket.close()
@@ -67,9 +80,10 @@ class Client:
         try:
             while True:
                 msg = input()
-                msg = Message("msg", datetime.now().strftime("%H:%M"), msg, self.nickname)
+                msg = Message("msg", datetime.now().strftime("%H:%M"), self.nickname, msg)
 
                 self.socket.send(pickle.dumps(msg))
+
         except (KeyboardInterrupt, SystemExit, EOFError):
             return
 
@@ -80,18 +94,24 @@ class Client:
                 data = self.socket.recv(1024) # gets data from the server in utf-8 format
 
                 if data:
-                    print(pickle.loads(data))
+                    msg = pickle.loads(data)
+
+                    print("[%s] %s: %s" % (datetime.now().strftime("%H:%M"), msg.nickname, msg.content))
+
+                    if pygetwindow.getActiveWindowTitle() != window_title:
+                        InteractableWindowsToaster("WhisperNet").show_toast(Toast([msg.nickname, msg.content]))
+
         except (ConnectionResetError, ConnectionAbortedError):
             print("[%s] Connection to the server lost" % datetime.now().strftime("%H:%M"))
 
 
 class Message:
-    def __init__(self, type, time, content, nickname):
+    def __init__(self, type, time, nickname, content):
         self.type = type
         self.time = time
-        self.content = content
         self.nickname = nickname
+        self.content = content
 
 
 if __name__ == "__main__":
-    main()
+    init()
