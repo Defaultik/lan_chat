7fbb78cdeb09d8cb8c387d656051e53651f84920
David <defaultik.personal@gmail.com>
2024-12-15 19:37:28 +0200

    # addinitonal checks; logging improved; handshake alghorytm improved; function to check if the second client is found

--- source/client.py
+++ source/client.py
@@ -39,47 +39,75 @@ class Client:
             logging.info("Connection with the server established!\n")
 
             await self.handshake()
-            # await asyncio.gather(self.receive(), self.send(), self.input_handler())
         except (asyncio.CancelledError, KeyboardInterrupt):
             self.writer.close()
-            self.writer.wait_closed()
+            await self.writer.wait_closed()
 
             logging.info("You disconnected from the server")
 
 
     async def handshake(self):
-        logging.info("Trying to generate encryption keys...")
-
         try:
             self.private_key, self.public_key = RSA.generate_keys()
             logging.info("Encryption keys successfully generated")
         except:
-            logging.error("Failed to generate encryption keys")
+            logging.error("Failed to generate encryption keys\n")
 
-            self.writer.close()
-            self.writer.wait_closed()
+            if self.writer:
+                self.writer.close()
+                await self.writer.wait_closed()
 
             logging.info("You disconnected from the server")
+            return
         
-        if self.writer:
+        try:
             self.writer.write(pickle.dumps(self.public_key))
             await self.writer.drain()
+            logging.info("Encryption key sent successfully\n")
+        except:
+            logging.error("Failed to send encryption key")
 
-            logging.info("Encryption key sent successfully")
+            self.writer.close()
+            await self.writer.wait_closed()
+
+            return
         
-            for _ in range(3):
-                logging.info("Waiting for server response...")
+        for _ in range(3):
+            logging.info("Waiting for server response...")
 
-                data = await self.reader.read(1024)
-                if data:
-                    data = pickle.loads(data)
+            try:
+                data = await asyncio.wait_for(self.reader.read(1024), timeout=5)
+            except asyncio.TimeoutError:
+                continue
 
-                    if data == "OK":
-                        logging.info("Encryption successfuly established")
-                        # TODO: Receive; Send functions to the server
-                        break
-                        
-                    await asyncio.sleep(2)
+            if not data:
+                break
+
+            try:
+                data = pickle.loads(data)
+            except pickle.UnpicklingError:
+                logging.error("Failed to deserialize server response")
+                continue
+
+            if data == "OK":
+                logging.info("Encryption successfuly established")
+
+                await self.lone_check()
+                await asyncio.gather(self.receive(), self.send(), self.input_handler())
+
+                break
+
+        if not self.writer.is_closing():
+            self.writer.close()
+            await self.writer.wait_closed()
+
+            logging.info("You disconnected from the server")
+
+
+    async def lone_check(self):
+        if self.writer:
+            self.writer.write(pickle.dumps("ALONE"))
+            await self.writer.drain()
 
         
     async def input_handler(self):
@@ -93,6 +121,7 @@ class Client:
     async def send(self):
         while True:
             msg = await self.message_queue.get()
+            msg = Message(msg)
             # msg = Message("msg", datetime.now().strftime("%H:%M"), self.nickname, encrypt(msg))
 
             if self.writer:
@@ -116,6 +145,8 @@ class Client:
 
                 if isinstance(data, Message):
                     logging.info(f"{data.nickname}: {data.content}")
+                else:
+                    logging.info(f"{data}")
             except (asyncio.IncompleteReadError, ConnectionResetError):
                 break
 
@@ -126,7 +157,8 @@ class Client:
 
 
 class Message:
-    pass
+    def __init__(self, content):
+        self.content = content
         
 
 if __name__ == "__main__":
